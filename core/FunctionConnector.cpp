ec0b2bec647607c5e623dc3cd0521d70fa38432e
Alexandr <a.kuzmuk@gmail.com>
2025-12-05 18:29:43 +0300

    fix

--- core/FunctionConnector.cpp
+++ core/FunctionConnector.cpp
@@ -72,7 +72,7 @@ void FunctionConnectionGuard::operator=(AbstractFunctionConnector::Connection &_
 
 void FunctionConnectionGuard::operator=(FunctionConnectionGuard &&_g) {
   if (c_) {
-    if (c_->type_ == AbstractFunctionConnector::Connection::Direct) c_->thread_->invokeMethod([_p = c_]() { delete _p; });
+    if (c_->thread_->id() == std::this_thread::get_id()) c_->thread_->invokeMethod([_p = c_]() { delete _p; });
     else { delete c_; }
   }
   c_ = _g.c_;
