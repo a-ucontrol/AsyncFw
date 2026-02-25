0740435e69364778ed4c5eb5f630a388a4179438
Alexandr <a.kuzmuk@gmail.com>
2026-02-25 10:00:39 +0300

    upd init_priority

--- instance.hpp
+++ instance.hpp
@@ -24,7 +24,7 @@ protected:
   void remove(AbstractInstance *);
 
 private:
-  inline static class List list __attribute__((init_priority(65532)));
+  inline static class List list __attribute__((init_priority(65531)));
 };
 
 template <typename T>
