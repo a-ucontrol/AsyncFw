8064fbd0313a4458233c5321a91645f26a642c1e
Alexandr <a.kuzmuk@gmail.com>
2026-02-28 10:07:19 +0300

    Revert "fix"
    This reverts commit 4e4f5290634f032fd13f03428cbdcdb3c53d1cba.

--- core/Thread.h
+++ core/Thread.h
@@ -156,10 +156,6 @@ protected:
   void exec();
 
 private:
-  inline static struct List : public std::vector<AbstractThread *> {
-    ~List();
-    std::mutex mutex;
-  } list __attribute__((init_priority(65531)));
   Private &private_;
 };
 
