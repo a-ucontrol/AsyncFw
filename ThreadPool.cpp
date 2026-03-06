12cacb0a19b6da1361b09492523464d780767392
Alexandr <a.kuzmuk@gmail.com>
2026-03-06 16:22:28 +0300

    fix destroy thread pool

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -80,15 +80,10 @@ AbstractThreadPool::Thread::Thread(const std::string &name, AbstractThreadPool *
 void AbstractThreadPool::Thread::destroy() {
   pool->removeThread(this);
   AbstractThread::quit();
-  AbstractTask *_t = new Task([p = this]() {
+  pool->thread_->invokeMethod([p = this]() {
     p->waitFinished();
     delete p;
   });
-  if (!pool->thread_->invokeTask(_t)) {
-    lsDebug() << LogStream::Color::Red << "pool thread not running" << '(' + pool->thread_->name() + ')';
-    _t->invoke();
-    delete _t;
-  }
   lsTrace();
 }
 
