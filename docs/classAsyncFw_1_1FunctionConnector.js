3f9bebd21e00f2089407d0917c73b76540ed95c7
Alexandr <a.kuzmuk@gmail.com>
2026-03-19 20:39:45 +0300

    WIP: doc

--- docs/classAsyncFw_1_1FunctionConnector.js
+++ docs/classAsyncFw_1_1FunctionConnector.js
@@ -1,7 +1,6 @@
 var classAsyncFw_1_1FunctionConnector =
 [
-    [ "connect", "classAsyncFw_1_1FunctionConnector.html#ab0c621be7a792e256acab8241ae563a0", null ],
-    [ "connect", "classAsyncFw_1_1FunctionConnector.html#a36849fd3a6d79bfc0aa1a14a2b7d4c56", null ],
     [ "operator()", "classAsyncFw_1_1FunctionConnector.html#a1ab25b31810087bc1d432073c5508f9a", null ],
+    [ "connect", "classAsyncFw_1_1FunctionConnector.html#a36849fd3a6d79bfc0aa1a14a2b7d4c56", null ],
     [ "operator()", "classAsyncFw_1_1FunctionConnector.html#ab78eb4bd213a04c987d1230c61e0f354", null ]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['connect_0',['connect',['../classAsyncFw_1_1FunctionConnector.html#ab0c621be7a792e256acab8241ae563a0',1,'AsyncFw::FunctionConnector::connect(T f, Connection::Type t=Connection::Default)'],['../classAsyncFw_1_1FunctionConnector.html#a36849fd3a6d79bfc0aa1a14a2b7d4c56',1,'AsyncFw::FunctionConnector::connect(O *o, M m, Connection::Type t=Connection::Default)']]],
+  ['connect_0',['connect',['../classAsyncFw_1_1FunctionConnector.html#a36849fd3a6d79bfc0aa1a14a2b7d4c56',1,'AsyncFw::FunctionConnector']]],
   ['currentthread_1',['currentThread',['../classAsyncFw_1_1AbstractThread.html#ac1b35ef04b06ce771f6d6ca0a5b120db',1,'AsyncFw::AbstractThread::currentThread()'],['../classAsyncFw_1_1Thread.html#a46662ffa7482f1c1e52b654c51639fd2',1,'AsyncFw::Thread::currentThread()']]]
 ];
