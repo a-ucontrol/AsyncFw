0602e143c80d5b93616f9a25bcb1bbeb256a39dd
Alexandr <a.kuzmuk@gmail.com>
2026-03-21 17:58:43 +0300

    upd

--- DataArrayTcpClient.cpp
+++ DataArrayTcpClient.cpp
@@ -5,6 +5,7 @@ This file is part of the AsyncFw project. Licensed under the MIT License.
 See {Link: LICENSE file https://mit-license.org} in the project root for full license information.
 */
 
+#include <algorithm>
 #include "core/LogStream.h"
 #include "Timer.h"
 #include "Coroutine.h"
