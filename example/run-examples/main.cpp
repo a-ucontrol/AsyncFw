e154ec6a61752df705e39af5d9ff7bf358649ad9
Alexandr <a.kuzmuk@gmail.com>
2026-04-26 14:25:21 +0300

    add DataArrayTcp to run-examples

--- example/run-examples/main.cpp
+++ example/run-examples/main.cpp
@@ -73,6 +73,10 @@ void run_examples(bool _socket) {
   logInfo() << "Start:" << app;
   process.start(app);
   process.wait();
+  app = EXAMPLES_PATH "DataArrayTcp";
+  logInfo() << "Start:" << app;
+  process.start(app);
+  process.wait();
 
   if (_socket) {
     app = EXAMPLES_PATH "SocketExample";
