1204344c089aefb3e0f4b857ffc68dfdaa18a57b
Alexandr <a.kuzmuk@gmail.com>
2026-04-30 15:47:09 +0300

    WIP: fix MainThread for Qt + doc

--- main/MainThread.h
+++ main/MainThread.h
@@ -162,7 +162,7 @@ private:
       for (std::vector<Timer>::iterator it = timers.begin(); it != timers.end(); ++it) {
         if (it->id == id) {
           if (it->qid >= 0) QObject::killTimer(it->qid);
-          _t = new Task([p = it->task] { delete p; });
+          _t = new Function([p = it->task] { delete p; });
           timers.erase(it);
           break;
         }
@@ -242,7 +242,7 @@ private:
       LockGuard lock = lockGuard();
       for (const std::shared_ptr<Poll> &poll : notifiers)
         if (poll->in.socket() == fd) {
-          _t = new Task([p = poll->task] { delete p; });
+          _t = new Function([p = poll->task] { delete p; });
           notifiers.removeAll(poll);
           break;
         }
