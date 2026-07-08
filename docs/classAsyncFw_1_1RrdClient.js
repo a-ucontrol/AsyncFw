c786f609aa004e3c6b7128fded0002e657337e4a
Alexandr <a.kuzmuk@gmail.com>
2026-07-08 18:34:11 +0300

    WIP: docs

---
+++ docs/classAsyncFw_1_1RrdClient.js
@@ -0,0 +1,11 @@
+var classAsyncFw_1_1RrdClient =
+[
+    [ "RrdClient", "classAsyncFw_1_1RrdClient.html#a99c8606f7788c0ab1d468b61b4de9adf", null ],
+    [ "~RrdClient", "classAsyncFw_1_1RrdClient.html#a9db5f1a5ddaa3c083851d557764937c0", null ],
+    [ "connectToHost", "classAsyncFw_1_1RrdClient.html#ab598862c2fcddef6f476db9d1ed9faf6", null ],
+    [ "connectToHost", "classAsyncFw_1_1RrdClient.html#ad8724d7772f037925d9694c1afc20d8e", null ],
+    [ "disconnectFromHost", "classAsyncFw_1_1RrdClient.html#a7437419c70ea6d55a6a701211b4582a2", null ],
+    [ "transmit", "classAsyncFw_1_1RrdClient.html#a4d6d253c85c993ed6906efe3ced940a7", null ],
+    [ "tlsSetup", "classAsyncFw_1_1RrdClient.html#ae7e51edcf279e6ecbfce7b4ca67dfe45", null ],
+    [ "disableTls", "classAsyncFw_1_1RrdClient.html#a70b7a42567a8b3fde089967a4f892541", null ]
+];
---
+++ docs/classAsyncFw_1_1RrdServer.js
@@ -0,0 +1,8 @@
+var classAsyncFw_1_1RrdServer =
+[
+    [ "RrdServer", "classAsyncFw_1_1RrdServer.html#a80621329e326c77a65c5e4e65efa511d", null ],
+    [ "~RrdServer", "classAsyncFw_1_1RrdServer.html#ad57e2c435b5730d56efe5615e31b0075", null ],
+    [ "quit", "classAsyncFw_1_1RrdServer.html#a36cdc4d3eb393265d357c727d31f18d5", null ],
+    [ "transmit", "classAsyncFw_1_1RrdServer.html#a518ff8e41bae78f1b1c0e099514e56eb", null ],
+    [ "tcpServer", "classAsyncFw_1_1RrdServer.html#a1d88715a189ea36663ca192b64886700", null ]
+];
