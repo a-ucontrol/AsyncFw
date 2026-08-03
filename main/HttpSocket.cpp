40ed2d66d56d5826da6f8fb1687cb04bdc512d48
Alexandr <a.kuzmuk@gmail.com>
2026-08-03 14:02:54 +0300

    upd logs

--- main/HttpSocket.cpp
+++ main/HttpSocket.cpp
@@ -10,6 +10,17 @@ See {Link: LICENSE file https://mit-license.org} in the project root for full li
 #include "core/LogStream.h"
 #include "HttpSocket.h"
 
+#ifdef EXTEND_SOCKET_TRACE
+  #define trace LogStream(+LogStream::Trace | LogStream::Black, __PRETTY_FUNCTION__, __FILE__, __LINE__, LS_DEFAULT_FLAGS | LOG_STREAM_CONSOLE_ONLY).output
+  #define warning_if(x) \
+if (x) LogStream(+LogStream::Warning | LogStream::Blue, __PRETTY_FUNCTION__, __FILE__, __LINE__, LS_DEFAULT_FLAGS | LOG_STREAM_CONSOLE_ONLY).output()
+#else
+  #define trace() \
+if constexpr (0) LogStream()
+  #define warning_if(x) \
+    if constexpr (0) LogStream()
+#endif
+
 #define SOCKET_WRITE_SIZE 8192
 
 using namespace AsyncFw;
@@ -53,7 +64,7 @@ void HttpSocket::activateEvent() {
 
 void HttpSocket::readEvent() {
   if (full_) clearReceived();
-  lsTrace();
+  trace();
 
   AsyncFw::DataArray &_da = peek();
 
