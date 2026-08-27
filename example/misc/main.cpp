a54cf7565d77339c44d58359659adc192defe12c
Alexandr <a.kuzmuk@gmail.com>
2026-08-27 10:32:14 +0300

    Revert "waiter eample in misc"
    This reverts commit 4dc1c64639f4f45a114e2e755b4835461ac98a2e.

--- example/misc/main.cpp
+++ example/misc/main.cpp
@@ -1,46 +1,11 @@
 #include <AsyncFw/MainThread>
 #include <AsyncFw/LogStream>
 
-#include <AsyncFw/Timer>
-
 int main(int argc, char *argv[]) {
-  AsyncFw::Thread::Waiter _w1;
-  AsyncFw::Thread::Waiter _w2;
+  AsyncFw::Thread::current()->invoke([]() { AsyncFw::MainThread::exit(); });
 
-  AsyncFw::Timer::single(50, []() { AsyncFw::MainThread::exit(); });
-
-  AsyncFw::Timer::single(1000, [&_w1, &_w2]() {
-    _w1.complete();
-    _w2.complete();
-  });
-
-  AsyncFw::Thread::current()->started.connect([&_w1, &_w2]() {
-    AsyncFw::Thread::current()->invoke([&_w1]() {
-      lsNotice() << "start wait 1";
-      _w1.wait();
-      lsNotice() << "end wait 1";
-    });
-    AsyncFw::Thread::current()->invoke([&_w2]() {
-      lsNotice() << "start wait 2";
-      _w2.wait();
-      lsNotice() << "end wait 2";
-    });
-  });
-
-  lsNotice() << "1 Start";
+  lsNotice() << "Start Application";
   int ret = AsyncFw::MainThread::exec();
-  lsNotice() << "1 End " << ret;
-
-  AsyncFw::Timer::single(50, []() { AsyncFw::MainThread::exit(); });
-
-  AsyncFw::Timer::single(1000, [&_w1, &_w2]() {
-    _w1.complete();
-    _w2.complete();
-  });
-
-  lsNotice() << "2 Start";
-  ret = AsyncFw::MainThread::exec();
-  lsNotice() << "2 End " << ret;
-
+  lsNotice() << "End Application " << ret;
   return ret;
 }
